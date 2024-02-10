ac8f8fb4a220e61824894979683d874e1f301b63
Jordan Pacheco <jordanrune@gmail.com>
2024-02-10 14:12:01 -0300

    nlw-expert-notes completo

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+import { App } from "./app";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Toaster } from "sonner";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <Toaster richColors />
+  </React.StrictMode>
+);
