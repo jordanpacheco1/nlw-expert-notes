d4c9def7d50207d7d9f0794b7fde36e56d08c542
Jordan Pacheco <jordanrune@gmail.com>
2024-02-11 18:00:29 -0300

    fix

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -35,6 +35,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
     if (event.target.value === "") {
       setShouldShowOnboarding(true);
+      setIsRecording(false);
     }
   }
 
